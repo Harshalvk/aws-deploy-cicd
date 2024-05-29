51d04bd4cf59e4473255576aa768a9e2812b7c11
Harshalvk <harshalvkhobragade@gmail.com>
2024-05-29 17:32:14 +0530

    change index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send("<h1>Server listening!!!</h1>")
+  return res.send("<h1>CI/CD working!</h1>")
 })
+
 
 app.listen(3000, () => {
   console.log('Server is listening!')
