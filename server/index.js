bd5aaed1764f0882f681b7e80cbc48aecad9aa93
Harshalvk <harshalvkhobragade@gmail.com>
2024-05-29 17:43:05 +0530

    add new route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   return res.send("<h1>CI/CD working!</h1>")
 })
 
+app.get("/success", (req, res) => {
+  return res.send("<h1>You have successfully implmented CI/CD!</h1>")
+})
 
 app.listen(3000, () => {
   console.log('Server is listening!')
